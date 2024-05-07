84c87bb89e273acccdc286b244d3afaa572b8695
kamilly-quadros <kamiquadros20@gmail.com>
2024-05-07 16:39:05 -0400

    Anatomia das Classes - 3

--- java-anatomia-classes/src/App.java
+++ java-anatomia-classes/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String primeiroNome = "Kamilly";
+        String segundoNome = "Quadros";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método "+primeiroNome.concat(" ").concat(segundoNome);
     }
 }
